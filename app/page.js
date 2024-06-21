d97dc4e8f6b1b46708b7588f1f306eb7e68df014
NWAKO DAVID <nwako onyeka>
2024-06-21 14:53:24 +0100

    initial setup

---
+++ app/page.js
@@ -0,0 +1,13 @@
+export default function Index(){
+  const myName ="onyeka";
+  return(
+    <div>
+      <h1>My name is {myName}</h1>
+      <p>I am learning reactjs</p>
+      <p>I am learning reactjs</p>
+      <p>I am learning reactjs</p>
+      <p>I am learning reactjs</p>
+      <p>I am learning reactjs</p>
+    </div>
+  )
+}
